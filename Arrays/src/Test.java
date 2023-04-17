5e0ebcb8e581a16f45174555a53530b57b108bb1
Nicu Taban <nictbndeveloper@gmail.com>
2023-04-17 15:57:31 +0300

    Arrays introduction

---
+++ Arrays/src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = -55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+        for (int anInt : intArray) {
+            System.out.println(anInt);
+        }
+    }
+}
