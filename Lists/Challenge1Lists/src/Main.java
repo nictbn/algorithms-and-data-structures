aca6a0070fd1ab65c9b35988a84c44bd9bf95cb0
Nicu Taban <nictbndeveloper@gmail.com>
2023-05-14 18:14:24 +0300

    Doubly Linked List Challenge 1 Description

--- Lists/Challenge1Lists/src/Main.java
+++ Lists/Challenge1Lists/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // Challenge 1: create an add before method for a doubly linked list
         Employee janeJones = new Employee("Jane", "Jones", 123);
         Employee johnDoe = new Employee("John", "Doe", 4567);
         Employee marySmith = new Employee("Mary", "Smith", 22);
