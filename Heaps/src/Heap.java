fd13a2471cd4b0b980799adaf6b0fabefb05161d
Nicu Taban <nictbndeveloper@gmail.com>
2023-05-28 22:03:39 +0300

    Heap delete

--- Heaps/src/Heap.java
+++ Heaps/src/Heap.java
@@ -13,7 +13,7 @@ public class Heap {
     public int getParent(int index) {
         return (index - 1) / 2;
     }
-    private void fixHEapAbove(int index) {
+    private void fixHeapAbove(int index) {
         int newValue = heap[index];
         while (index > 0 && newValue > heap[getParent(index)]) {
             heap[index] = heap[getParent(index)];
@@ -26,7 +26,67 @@ public class Heap {
             throw new IndexOutOfBoundsException("Heap is full");
         }
         heap[size] = value;
-        fixHEapAbove(size);
+        fixHeapAbove(size);
         size++;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
+    }
+
+    public int delete(int index) {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            fixHeapAbove(index);
+        }
+        size--;
+        return deletedValue;
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        // lastHeapIndex is used only in Heap Sort
+        int childToSwap;
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+            if (leftChild <= lastHeapIndex) {
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                } else {
+                    childToSwap = heap[leftChild] > heap[rightChild] ? leftChild : rightChild;
+                }
+                if (heap[index] < heap[childToSwap]) {
+                    int temp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = temp;
+                } else {
+                    break;
+                }
+                index = childToSwap;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i]);
+            System.out.print(", ");
+        }
+        System.out.println();
     }
 }
