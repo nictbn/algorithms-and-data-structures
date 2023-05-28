8856859845de2bcb354777f2062d97bbe931d2c7
Nicu Taban <nictbndeveloper@gmail.com>
2023-05-28 18:51:47 +0300

    Adds delete and treats nodes with 0 or 1 children

--- Trees/BinarySearchTree/src/Tree.java
+++ Trees/BinarySearchTree/src/Tree.java
@@ -35,4 +35,27 @@ public class Tree {
         }
         return root.max();
     }
+
+    public void delete(int value) {
+        root = delete(root, value);
+    }
+
+    private TreeNode delete(TreeNode subTreeRoot, int value) {
+        if (subTreeRoot == null) {
+            return null;
+        }
+        if (value < subTreeRoot.getData()) {
+            subTreeRoot.setLeftChild(delete(subTreeRoot.getLeftChild(), value));
+        } else if (value > subTreeRoot.getData()) {
+            subTreeRoot.setRightChild(delete(subTreeRoot.getRightChild(), value));
+        } else {
+            // cases 1 and 2; node to delete has 0 or 1 children
+            if (subTreeRoot.getLeftChild() == null) {
+                return subTreeRoot.getRightChild();
+            } else if (subTreeRoot.getRightChild() == null) {
+                return subTreeRoot.getLeftChild()
+            }
+        }
+        return subTreeRoot;
+    }
 }
