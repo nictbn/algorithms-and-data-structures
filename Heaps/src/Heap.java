1a535961f6b9c392f53b7e7831671a80ca0febb5
Nicu Taban <nictbndeveloper@gmail.com>
2023-05-28 21:32:00 +0300

    Heap Insert

---
+++ Heaps/src/Heap.java
@@ -0,0 +1,32 @@
+public class Heap {
+    private int[] heap;
+    private int size;
+
+    public Heap(int capacity) {
+        heap = new int[capacity];
+    }
+
+    public boolean isFull() {
+        return size == heap.length;
+    }
+
+    public int getParent(int index) {
+        return (index - 1) / 2;
+    }
+    private void fixHEapAbove(int index) {
+        int newValue = heap[index];
+        while (index > 0 && newValue > heap[getParent(index)]) {
+            heap[index] = heap[getParent(index)];
+            index = getParent(index);
+        }
+        heap[index] = newValue;
+    }
+    public void insert(int value) {
+        if (isFull()) {
+            throw new IndexOutOfBoundsException("Heap is full");
+        }
+        heap[size] = value;
+        fixHEapAbove(size);
+        size++;
+    }
+}
