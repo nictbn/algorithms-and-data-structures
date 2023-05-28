97c36152b5c661b0bf767063bf4350cdd8d6706f
Nicu Taban <nictbndeveloper@gmail.com>
2023-05-28 18:16:02 +0300

    Initial Binary Search Tree Implementation

---
+++ Trees/BinarySearchTree/src/Tree.java
@@ -0,0 +1,11 @@
+public class Tree {
+    TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        } else {
+            root.insert(value);
+        }
+    }
+}
